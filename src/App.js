c5a141bdbc65775f3b615a88289b04ef350a208c
Richard Nwokoye <rnwokoye@deloitte.com>
2020-05-07 18:09:17 -0400

    changed welcome text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello There, Bye there! React was changed 
+          REACT IS UP AND RUNNING NOW!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React....develop interactive front end Webapps easy
         </a>
       </header>
     </div>
